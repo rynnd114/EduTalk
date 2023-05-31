27088ae96a08d3431ac639a9eb98d9209c210c08
rynnd114 <alraqkiananda@student.unmul.co.id>
2023-05-31 17:28:37 +0800

    first project

---
+++ app/src/main/java/com/bangkit/edutalk/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.bangkit.edutalk.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
